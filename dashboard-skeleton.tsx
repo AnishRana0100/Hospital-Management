4d70400d262b58b5dce6cf7e65baf63a1bb1cd98
Anish Rana <anishrana1096@gmail.com>
2025-04-11 14:41:37 +0530

    Code taken from Visual Studio

---
+++ dashboard-skeleton.tsx
@@ -0,0 +1,16 @@
+import { Skeleton } from "@/components/ui/skeleton"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
+
+export default function DashboardSkeleton() {
+  return (
+    <Card>
+      <CardHeader className="pb-2">
+        <Skeleton className="h-6 w-1/3" />
+        <Skeleton className="h-4 w-1/2 mt-2" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="h-[200px] w-full" />
+      </CardContent>
+    </Card>
+  )
+}
